84818223686f40602bb7d22199fe2a0abce525ee
rakhi deokate <devkaterakhigmail.com>
2021-02-04 16:41:37 +0530

    channel
    I created chat channel.

--- Rakhi/Assignment3-message/config/routes.rb
+++ Rakhi/Assignment3-message/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home/Index'
   resources :messages
+
+  mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
