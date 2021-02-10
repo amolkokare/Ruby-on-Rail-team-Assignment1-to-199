55b8cf06c7ab1fc6a253effbf26a40544fdb4d10
rakhi deokate <devkaterakhigmail.com>
2021-02-10 16:29:56 +0530

    Assignment4-Hospital Man Sys
    here, I used pg db. I added doctor record, Patient record, appointment details.

--- Rakhi/assignment4-Hospital_Man_Sys/db/schema.rb
+++ Rakhi/assignment4-Hospital_Man_Sys/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_10_084744) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string "issue"
     t.date "date"
