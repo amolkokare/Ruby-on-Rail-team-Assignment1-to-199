05d1e935755fdc27b861f5bcd364a916c360c0d0
rakhi deokate <devkaterakhigmail.com>
2021-02-10 11:22:15 +0530

    Message App
    I complete my Assignment3- Message App using scaffolding,also I used marquee on message app.

--- Rakhi/Assignment3-Message App/app/controllers/posts_controller.rb
+++ Rakhi/Assignment3-Message App/app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post = Post.user.new(post_params)
+  	@post = Post.new(post_params)
 
   	if @post.save
   	  redirect_to root_path
