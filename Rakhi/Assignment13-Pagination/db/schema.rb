83122fa14b9794546f894f9cae1f0be0fb240835
rakhi deokate <devkaterakhi@gmail.com>
2021-02-13 14:19:42 +0530

    Assignment13-Pagination
    pagination per 7 applied. 50,000 users database are access.

--- Rakhi/Assignment13-Pagination/db/schema.rb
+++ Rakhi/Assignment13-Pagination/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210326020905) do
+ActiveRecord::Schema.define(version: 20210104102231) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
