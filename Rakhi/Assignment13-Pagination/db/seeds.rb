c97646f1e0556279a3538fbd3209de7d28084059
rakhi deokate <devkaterakhi@gmail.com>
2021-02-19 16:57:08 +0530

    Assignment13-Pagination
    some changes in gemfile.

--- Rakhi/Assignment13-Pagination/db/seeds.rb
+++ Rakhi/Assignment13-Pagination/db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-50_000.times do |i|
-  puts "Creating user #{i + 1}/50_000" if (i + 1) % 100 == 0
+10_000.times do |i|
+  puts "Creating user #{i + 1}/10_000" if (i + 1) % 100 == 0
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
