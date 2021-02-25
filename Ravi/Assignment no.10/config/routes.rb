c53bca88dce59655c4602cc749b9f7e0085d395c
Ravi-Londhe <74483044+Ravi-Londhe@users.noreply.github.com>
2021-02-25 16:31:14 +0530

    clone the website using css and bootstrap

--- Ravi/Assignment no.10/config/routes.rb
+++ Ravi/Assignment no.10/config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "posts#index"
+   devise_for :users
 
-  # Nested resource routes give us access to replies from each post
-  resources :posts do
+    #GET    /users/sign_up
+   root "posts#index"
+   
+   #root "replies#form"
+   
+
+
+   #root "users/sign_up#index"
+   #root "users#sign_up"
+   #root to: "users#index"
+   #root "users#index"
+   
+
+
+
+
+   #users/sign_up
+
+   # Nested resource routes give us access to replies from each post
+    resources :posts do
   	resources :replies
   end
 end
