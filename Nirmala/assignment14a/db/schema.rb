c7615d139d6d48a4aa7e9d55520fbc58a4dc4b28
nirmalaraut <rautnirmala1999@gmail.com>
2021-03-03 11:45:13 +0530

    imported chiptole data
    assignment14

--- Nirmala/assignment14a/db/schema.rb
+++ Nirmala/assignment14a/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_25_112513) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.integer "order"
     t.integer "quantity"
