7735ec13d874a1c7755c945ff9a4bb7bca2d3628
rakhi-deokate <76157209+rakhi-deokate@users.noreply.github.com>
2021-03-08 16:03:06 +0530

    Delete Rakhi/facebookwall directory

--- Rakhi/facebookwall/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  has_many :replies, dependent: :destroy
-  validates :title, :body, presence: true
-    
-end
--- Rakhi/facebookwall/db/migrate/20210306110224_add_image_to_reply.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToReply < ActiveRecord::Migration[6.1]
-  def change
-    add_column :replies, :image, :string
-  end
-end
