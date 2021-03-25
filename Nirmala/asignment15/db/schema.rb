6f6d58526aa349cc033290dfd40060ebe3112c93
nirmalaraut <rautnirmala1999@gmail.com>
2021-03-25 12:53:25 +0530

    Nutrition API
    assignment-6

--- Nirmala/asignment15/db/schema.rb
+++ Nirmala/asignment15/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_02_065116) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "revenues", force: :cascade do |t|
     t.string "item"
     t.integer "total_sales"
