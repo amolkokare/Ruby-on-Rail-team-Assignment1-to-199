8f6cfed58ade0c95f7752e474c1bff325f9020f6
Ravi-Londhe <74483044+Ravi-Londhe@users.noreply.github.com>
2021-04-07 14:45:26 +0530

    as17

--- Ravi/Assignment no.17/app/mailers/application_mailer.rb
+++ Ravi/Assignment no.17/app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-
-  default from: "'ravi' <from@example.com>"
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- Ravi/Assignment no.17/config/environments/development.rb
+++ Ravi/Assignment no.17/config/environments/development.rb
@@ -30,6 +30,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -73,18 +79,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain:"railscasts.com",
-    authentication: :plain,
-    enable_starttls_auto: true,
-    user_name: ENV["londheravindra98@gmail.com"],
-    password: ENV["#NPW&C628@"]
-
-  }
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
 end
