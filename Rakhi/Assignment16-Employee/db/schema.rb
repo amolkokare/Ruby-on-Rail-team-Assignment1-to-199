2ca5bae1e6f0f09073bc2e18658c618cf597fe51
rakhi deokate <>
2021-04-08 11:53:36 +0530

    Assignment16-Employee

--- Rakhi/Assignment16-Employee/db/schema.rb
+++ Rakhi/Assignment16-Employee/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_050830) do
+ActiveRecord::Schema.define(version: 2021_03_08_063727) do
 
   create_table "companies", force: :cascade do |t|
     t.string "cname"
